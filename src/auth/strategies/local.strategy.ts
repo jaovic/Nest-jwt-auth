b6fd3f37c4ea388848549d6fc8a727150cc1a269
João Victor Martins <j_victorfernandes@hotmail.com>
2022-08-09 13:18:28 -0300

    ✨ UseGuard strategies

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,21 @@
+import { MessagesHelper } from 'src/helpers/message.helper';
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../auth.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+@Injectable()
+export class localStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      usernameField: 'email',
+    });
+  }
+  async validate(email: string, password: string) {
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException(MessagesHelper.PASSWORD_OR_EMAIL_INVALID);
+    }
+    return user;
+  }
+}
