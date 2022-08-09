580b36e776e18ded60b97a3027409f61ef5bc8cd
João Victor Martins <j_victorfernandes@hotmail.com>
2022-08-09 13:19:15 -0300

    ✨ helpers

---
+++ src/helpers/message.helper.ts
@@ -0,0 +1,12 @@
+export const MessagesHelper = {
+  PASSWORD_VALID:
+    'The password must contain uppercase and lowercase letters, numbers and special characters',
+  EMAIL_REQUIRED: 'Email is required',
+  EMAIL_INVALID: 'Email is invalid',
+  NAME_REQUIRED: 'Name is required',
+  NAME_INVALID: 'Name is invalid',
+  PASSWORD_REQUIRED: 'Password is required',
+  CPF_REQUIRED: 'CPF is required',
+  CPF_INVALID: 'CPF is invalid',
+  PASSWORD_OR_EMAIL_INVALID: 'Password or email is invalid',
+};
